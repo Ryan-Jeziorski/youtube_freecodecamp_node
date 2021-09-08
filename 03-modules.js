c0876d347c2f4bc01c93d60ac19225a1ce38fe51
Ryan <Rjeziorski@outlook.com>
2021-09-08 07:54:33 -0500

    use alternative export in main

--- 03-modules.js
+++ 03-modules.js
@@ -3,9 +3,12 @@
 // and ./05-utils for function
 
 //requires
-const names = require('./04-names.js'); //for names of variables
+const names = require('./04-names'); //for names of variables
 const sayHi = require('./05-utils');    //for sayHi utility
+const data = require('./06-alternate-export')
+console.log(data)
 
+sayHi(data.singlePerson.name);
 sayHi(names.john);
 sayHi(names.peter);
 sayHi('susan');
