244829e121379608fafd33bad8c13a11abec60c5
Ryan <Rjeziorski@outlook.com>
2021-09-09 02:12:36 -0500

    Add printline

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -8,6 +8,9 @@ console.log('start');
 const first = readFileSync('./content/first.txt', 'utf8');
 const second = readFileSync('./content/second.txt', 'utf8');
 
+//print the contents of the files
+console.log(first, second)
+
 //write to file
 writeFileSync(
     './content/result-sync.txt',
