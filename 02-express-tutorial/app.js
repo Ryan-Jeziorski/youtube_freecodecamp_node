1ea8570c7144ed48f66b8bdce1597a177b97174a
ryan <rjeziorski@outlook.com>
2021-10-23 21:23:39 -0500

    Created home page routing stuff

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,33 @@
-console.log('Express Tutorial');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    //if user hits the home page
+    if (url === '/'){
+        console.log(req.method);
+        console.log(req.url);
+
+        res.writeHead(200, {'content-type':'text/html'});
+        console.log('user hit the home page');
+        res.write('<h1>home page</h1>');
+        res.end();
+
+    } else if (url === '/about'){ 
+        console.log(req.method);
+        console.log(req.url);
+        res.writeHead(200, {'content-type':'text/html'});
+        console.log('user hit the abouit page');
+        res.write('<h1>about page</h1>');
+        res.end();
+
+    } else {  // otherwise
+        console.log(req.method);
+        console.log(req.url);
+        res.writeHead(200, {'content-type':'text/html'});
+        console.log('user hit the other page');
+        res.write('<h1>not the home page</h1>');
+        res.end();
+    }
+});
+
+server.listen(5000);
