512099414df6f9a3d34ba7e02741155a8aa30a2d
ryan <rjeziorski@outlook.com>
2021-10-24 23:39:58 -0500

    Convert to useing express to server static content

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,31 +1,25 @@
-//Import express
+//Import express module
 const express = require('express');
+//Import path module
+const path = require('path');
+
 //invoke express function to create server
 const app = express();
 
-// Most common express methods
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
+// Import static elements from public directory
+app.use(express.static('./public'));
 
-app.get('/', (req,res) => {
-    console.log('User hit the home page')
-    res.status(200).send('Home Page');
-});
+// Retrieve homepage 
+// app.get('/', (req,res) => {
+//     res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'));
+// });
 
-app.get('/about', (req,res) => {
-    console.log('User hit the about page')
-    res.status(200).send('About Page');
-});
-
+// If user is attempting to access a resource that doesn't exist
 app.all('*', (req,res) => {
-    res.status(404).send('<h1>resource not found</h1>');
+    res.status(404).send('error 404');
 });
 
+//Set server to lisen on port 5000
 app.listen(5000, () => {
     console.log('Server is listening on port 5000');
 });
